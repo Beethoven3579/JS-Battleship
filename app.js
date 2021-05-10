0e403522586e14cccba25e4c181cb50d6200421f
nickpaolino <nick_paolino@yahoo.com>
2021-05-09 21:41:22 -0400

    completed model object

--- app.js
+++ app.js
@@ -14,11 +14,41 @@ let view = {
     } 
 };
 
-view.displayHit("34");
-view.displayHit("12");
-view.displayHit("26");
-view.displayMiss("00");
-view.displayMiss("55");
-view.displayMiss("25");
+let model = {
+    boardsize: 7,
+    numShips: 3,
+    shipLength: 3, 
+    shipsSunk: 0,
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
+            {locations: ["24", "34", "44"], hits: ["", "", ""] },
+            {locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT");
+                if (this.isSunk(ship)){
+                    view.displayMessage("You sank my battleship!")
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
 
-view.displayMessage("Tap, Tap, is this thing on?")
+    isSunk: function(ship) {
+        for (let i = 0; i <  this.shipLength; i ++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+
+};
